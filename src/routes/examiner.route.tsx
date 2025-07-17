8955d2d054aad0dfcd998e00971a8403a69bfaff
danh-pham235 <phamdanh0305@gmail.com>
2025-07-17 19:45:15 +0700

    add .env and change examiner route

--- src/routes/examiner.route.tsx
+++ src/routes/examiner.route.tsx
@@ -1,14 +1,29 @@
 import type { RouteObject } from "react-router-dom";
-import ExaminerPage from "../page/Examiner";
 import ExaminerLayout from "../layouts/ExaminerLayout";
+import UploadSubmissionsPage from "../page/Examiner/UploadSubmissions";
+import RegradeDashboardPage from "../page/Examiner/RegradeDashboard";
+import UploadExamWithBaremPage from "../page/Examiner/UploadExamWithBarem";
+import { Navigate } from "react-router-dom";
 
 export const examinerRoutes: RouteObject = {
   path: "/examiner",
   element: <ExaminerLayout />,
   children: [
     {
-      path: "*",
-      element: <ExaminerPage />,
+      index: true,
+      element: <Navigate to="upload-submissions" replace />,
+    },
+    {
+      path: "upload-submissions",
+      element: <UploadSubmissionsPage />,
+    },
+    {
+      path: "regrade-dashboard",
+      element: <RegradeDashboardPage />,
+    },
+    {
+      path: "upload-exam-barem",
+      element: <UploadExamWithBaremPage />,
     },
   ],
 };

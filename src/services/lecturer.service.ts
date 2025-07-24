8d708181d52b724bd59ba7dd89b6619165c406af
machgiahao <machgiahao0979@gmail.com>
2025-07-24 11:43:50 +0700

    Update lecturer service

--- src/services/lecturer.service.ts
+++ src/services/lecturer.service.ts
@@ -1,14 +1,21 @@
 import axiosInstance from "../config/axiosConfig";
-import { API_DISTRIBUTION, API_SUBMISSION, API_EXAM } from "../constants/apiConstants";
-
+import {
+  API_DISTRIBUTION,
+  API_SUBMISSION,
+  API_EXAM,
+} from "../constants/apiConstants";
 
 export const getAssignedDistributions = async (examId: string) => {
-  const response = await axiosInstance.get(`${API_DISTRIBUTION.ASSIGNED}?examId=${examId}`);
+  const response = await axiosInstance.get(
+    `${API_DISTRIBUTION.ASSIGNED}?examId=${examId}`
+  );
   return response.data;
 };
 
 export const getSubmissionDetail = async (submissionId: string) => {
-  const response = await axiosInstance.get(`${API_SUBMISSION.GET_SUBMISSION_DETAIL}/${submissionId}`);
+  const response = await axiosInstance.get(
+    `${API_SUBMISSION.GET_SUBMISSION_DETAIL}/${submissionId}`
+  );
   return response.data;
 };
 
@@ -17,7 +24,11 @@ export const getBarem = async (examId: string) => {
   return response.data;
 };
 
-export const submitGrade = async (submissionId: string, grade: number, note: string) => {
+export const submitGrade = async (
+  submissionId: string,
+  grade: number,
+  note: string
+) => {
   const response = await axiosInstance.post(
     `${API_SUBMISSION.SUBMIT_GRADE}/${submissionId}`,
     {
@@ -33,6 +44,8 @@ export const getAllExam = async () => {
   return response.data;
 };
 export const aiGrade = async (submissionId: string) => {
-  const response = await axiosInstance.get(`${API_SUBMISSION.AI_SCORE}/${submissionId}`);
+  const response = await axiosInstance.post(`${API_SUBMISSION.AI_SCORE}`, {
+    submissionId,
+  });
   return response.data;
-}
+};
